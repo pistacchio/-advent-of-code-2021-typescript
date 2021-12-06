4bce7528169ba0a6a5e4dab2d43336968cebd3aa
pistacchio <pistacchio@gmail.com>
2021-12-06 15:19:39 +0100

    Day 6 part 2

--- 6.ts
+++ 6.ts
@@ -3,10 +3,8 @@ import { groupBy } from 'lodash';
 
 const INPUT_FILE = '/data/6.txt';
 
-const READY_TO_GIVE_BIRTH = 0;
-const JUST_GAVE_BIRTH = 6;
-const JUST_BORN = 8;
-const DAYS = 256;
+const PART_1_DAYS = 80;
+const PART_2_DAYS = 256;
 
 const input = fs
   .readFileSync(__dirname + INPUT_FILE)
@@ -17,23 +15,43 @@ const input = fs
   .filter(Boolean)
   .map(Number);
 
-function part1(input: number[]): number {
-  return Array.from({ length: DAYS }).reduce<number[]>(
-    (school) =>
-      school.flatMap((fish) =>
-        fish === READY_TO_GIVE_BIRTH
-          ? [JUST_GAVE_BIRTH, JUST_BORN]
-          : [fish - 1],
-      ),
-    input,
-  ).length;
-}
+function part1And2(input: number[], days: number): number {
+  const fishesByAge = {
+    ...{
+      '0': 0,
+      '1': 0,
+      '2': 0,
+      '3': 0,
+      '4': 0,
+      '5': 0,
+      '6': 0,
+      '7': 0,
+      '8': 0,
+    },
+    ...Object.fromEntries(
+      Object.entries(groupBy(input)).map(([k, v]) => [k, v.length]),
+    ),
+  };
 
-function part2(input: number[]): number {
-  return 2;
+  const school = Array.from({ length: days }).reduce<object>(
+    (school) => ({
+      0: school['1'],
+      1: school['2'],
+      2: school['3'],
+      3: school['4'],
+      4: school['5'],
+      5: school['6'],
+      6: school['7'] + school['0'],
+      7: school['8'],
+      8: school['0'],
+    }),
+    fishesByAge,
+  );
+
+  return Object.values(school).reduce((a, b) => a + b, 0);
 }
 
 export function run(): void {
-  console.log('Part 1:', part1(input));
-  console.log('Part 2:', part2(input));
+  console.log('Part 1:', part1And2(input, PART_1_DAYS));
+  console.log('Part 2:', part1And2(input, PART_2_DAYS));
 }
